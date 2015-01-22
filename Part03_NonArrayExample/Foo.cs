55e8279aedd6a48af433ff72f24aaf265e4eee6c
uoi100 <calvintruong11@hotmail.com>
2015-01-22 03:07:16 -0800

    Added Part 03

---
+++ Part03_NonArrayExample/Foo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part03_NonArrayExample
+{
+    class Foo
+    {
+        /// <summary>
+        /// Description: Integer indexer that returns the value squared.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int this[int value]
+        {
+            get
+            {
+                return value * value;
+            }
+        }
+    }
+}
---
+++ Part03_NonArrayExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part03_NonArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo foo = new Foo();
+
+            for (int i = 0; i <= 10; i++)
+                Console.WriteLine(foo[i]);
+
+            Console.Read();
+        }
+    }
+}
