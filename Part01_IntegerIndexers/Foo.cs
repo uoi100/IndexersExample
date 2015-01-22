36a939c3b6aeb7296fecaca44ec59fa0e8a453d2
uoi100 <calvintruong11@hotmail.com>
2015-01-22 00:41:29 -0800

    adding missing files.

---
+++ Part01_IntegerIndexers/Foo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part01_IntegerIndexers
+{
+    class Foo
+    {
+        private int[] bar;
+        private bool error;
+
+        /// <summary>
+        /// Description: Constructor, bar is allocated some space
+        /// </summary>
+        public Foo()
+        {
+            bar = new int[10];
+        }
+
+        /// <summary>
+        /// Description: Constructor, bar is allocated to the size
+        /// </summary>
+        /// <param name="size">Size of the array</param>
+        public Foo(int size)
+        {
+            bar = new int[size];
+        }
+
+        /// <summary>
+        /// Description: Integer indexer
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public int this[int number]
+        {
+            get
+            {
+                if (validateBounds(number))
+                    return bar[number];
+                return 0;
+            }
+            set
+            {
+                if (validateBounds(number))
+                    bar[number] = value;
+            }
+        }
+
+        /// <summary>
+        /// Description: Validate if the number is within bounds,
+        /// if it is then return true, else return false.
+        /// The error variable is also set to determine the result of the
+        /// the validation.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public bool validateBounds(int number)
+        {
+            if (number >= 0 && number < bar.Length)
+            {
+                error = false;
+                return true;
+            }
+
+            error = true;
+            return false;
+        }
+
+        /// <summary>
+        /// Description: Returns if the current
+        /// </summary>
+        /// <returns>Returns if there is an error or not</returns>
+        public bool isGood(){ return !error; }
+    }
+}
---
+++ Part01_IntegerIndexers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part01_IntegerIndexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo foo = new Foo();
+
+            foo[0] = 21;
+
+            foo[51] = 11;
+
+            // This will not throw an exception because of the indexer's fail-soft array
+            Console.WriteLine(foo[0]);
+            Console.WriteLine(foo[51]);
+            Console.WriteLine(foo[53]);
+            Console.Read();
+        }
+    }
+}
